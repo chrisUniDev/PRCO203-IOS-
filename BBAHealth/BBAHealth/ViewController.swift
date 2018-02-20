4b601c528573a27198f6e936b2024897496a7d74
Christopher Lehrer <christopher.lehrer@students.plymouth.ac.uk>
2018-02-20 19:39:21 +0000

    Cleaned up Viewcontroller Script

--- BBAHealth/BBAHealth/ViewController.swift
+++ BBAHealth/BBAHealth/ViewController.swift
@@ -15,51 +15,41 @@ class ViewController: UIViewController {
     
     var imageArray = [UIImage]()
     
+    let imageWidth: CGFloat = 330
+    let imageHeight: CGFloat = 400
+    let buttonSepationPadding: CGFloat = 60
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
         scrollView.frame = view.frame
-        
         imageArray = [#imageLiteral(resourceName: "InfoView_TitleImage_1"),#imageLiteral(resourceName: "InfoView_TitleImage_2")]
       
         for i in 0..<imageArray.count {
-            
             
             let buttonView = UIButton()
             
             buttonView.setImage(imageArray[i], for: .normal)
             buttonView.contentMode = .scaleAspectFit
             
-            let yPosition = 460 * CGFloat(i)
-            buttonView.frame = CGRect(x: 0, y: yPosition, width: 330, height: 400)
+            let yPosition = imageHeight + buttonSepationPadding * CGFloat(i)
+            buttonView.frame = CGRect(x: 0, y: yPosition, width: imageWidth, height: imageHeight)
             
             scrollView.contentSize.height = scrollView.frame.height * CGFloat(i + 1)
             scrollView.addSubview(buttonView)
            
-            buttonView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: (view.frame.width - 330)/2).isActive = true
-            //buttonView.contentHorizontalAlignment = UIControlContentHorizontalAlignmentCenter;
+            buttonView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: (view.frame.width - imageWidth)/2).isActive = true
 
             buttonView.layer.cornerRadius = 14
             buttonView.layer.masksToBounds = true
-            
-            
-  
         }
-        
-        
-        
     }
-    
-
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
 
