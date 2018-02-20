1f9776e6981766b453d378eb1624e15aa345b542
Christopher Lehrer <christopher.lehrer@students.plymouth.ac.uk>
2018-02-20 17:56:38 +0000

    Added images to the scroll view using an array
    the more images you have the larger the scroll view will become

--- BBAHealth/BBAHealth/ViewController.swift
+++ BBAHealth/BBAHealth/ViewController.swift
@@ -11,9 +11,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    var imageArray = [UIImage]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        scrollView.frame = view.frame
+        
+        imageArray = [#imageLiteral(resourceName: "InfoView_TitleImage_1"),#imageLiteral(resourceName: "InfoView_TitleImage_2")]
+      
+        for i in 0..<imageArray.count {
+            
+            let imageView = UIImageView()
+            imageView.image = imageArray[i]
+            imageView.contentMode = .scaleAspectFit
+            let yPosition = self.view.frame.height * CGFloat(i)
+            imageView.frame = CGRect(x: 0, y: yPosition, width: self.scrollView.frame.width, height: self.scrollView.frame.height)
+            
+            scrollView.contentSize.height = scrollView.frame.height * CGFloat(i + 1)
+            scrollView.addSubview(imageView)
+            
+            
+            
+        }
+        
+        
         
     }
     
