eb1d3aa6d35fda73ba2df78012013c2a7e91d2db
Christopher Lehrer <christopher.lehrer@students.plymouth.ac.uk>
2018-02-20 21:57:57 +0000

    Updated temp images

--- BBAHealth/BBAHealth/ViewController.swift
+++ BBAHealth/BBAHealth/ViewController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 
-
 class ViewController: UIViewController {
 
     @IBOutlet weak var scrollView: UIScrollView!
@@ -25,13 +24,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         scrollView.frame = view.frame
         
-        imageArray = [#imageLiteral(resourceName: "InfoView_TitleImage_1"),#imageLiteral(resourceName: "InfoView_TitleImage_1"),#imageLiteral(resourceName: "InfoView_TitleImage_1"),#imageLiteral(resourceName: "InfoView_TitleImage_1"),#imageLiteral(resourceName: "InfoView_TitleImage_1"),#imageLiteral(resourceName: "InfoView_TitleImage_1"),#imageLiteral(resourceName: "InfoView_TitleImage_1"),#imageLiteral(resourceName: "InfoView_TitleImage_1")]
-      
-        for i in 0..<imageArray.count {
+        imageArray = [#imageLiteral(resourceName: "InfoView_TitleImage_1"),#imageLiteral(resourceName: "InfoView_TitleImage_2"),#imageLiteral(resourceName: "InfoView_TitleImage_1"),#imageLiteral(resourceName: "InfoView_TitleImage_2"),#imageLiteral(resourceName: "InfoView_TitleImage_1"),#imageLiteral(resourceName: "InfoView_TitleImage_2"),#imageLiteral(resourceName: "InfoView_TitleImage_1"),#imageLiteral(resourceName: "InfoView_TitleImage_2")]
+        
+        createButtons(buttons: imageArray)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    private func createButtons(buttons: [UIImage]){
+        
+        for i in 0..<buttons.count {
             
             let buttonView = UIButton()
             
-            buttonView.setImage(imageArray[i], for: .normal)
+            buttonView.setImage(buttons[i], for: .normal)
             buttonView.contentMode = .scaleAspectFit
             
             let yPosition = buttonSepationPadding * CGFloat(i)
@@ -41,17 +51,13 @@ class ViewController: UIViewController {
             scrollView.addSubview(buttonView)
             
             buttonView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: (view.frame.width - imageWidth)/2).isActive = true
-
+            
+            //Creates the corners on each button image
             buttonView.layer.cornerRadius = 14
             buttonView.layer.masksToBounds = true
         }
-        
+        //Removes unwanted bottom white space
         scrollView.contentSize.height = scrollView.contentSize.height - 160
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
 
