f120e1f0c582e6687557b07ca0344498846fb407
Christopher Lehrer <christopher.lehrer@students.plymouth.ac.uk>
2018-02-20 15:57:43 +0000

    Created Initial Project Files
    Generated a new XCode Project

---
+++ BBAHealth/BBAHealthTests/BBAHealthTests.swift
@@ -0,0 +1,36 @@
+//
+//  BBAHealthTests.swift
+//  BBAHealthTests
+//
+//  Created by Christopher Lehrer on 20/02/2018.
+//  Copyright © 2018 Episteme. All rights reserved.
+//
+
+import XCTest
+@testable import BBAHealth
+
+class BBAHealthTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
