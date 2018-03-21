6227e092db09f43beadbd375f56fa0cc87f3f431
Christopher Lehrer <christopher.lehrer@students.plymouth.ac.uk>
2018-03-21 15:46:40 +0000

    Added siri wave

---
+++ BBAHealth/BBAHealth/Controllers/AudioWave.swift
@@ -0,0 +1,18 @@
+//
+//  AudioWave.swift
+//  BBAHealth
+//
+//  Created by Christopher Lehrer on 21/03/2018.
+//  Copyright © 2018 Episteme. All rights reserved.
+//
+
+import UIKit
+import SwiftSiriWaveformView
+
+class AudioWave: UIViewController {
+    
+    
+
+ 
+
+}
