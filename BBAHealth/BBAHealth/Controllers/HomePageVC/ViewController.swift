78ba62b96e7384358d536696167da340329b1c08
Christopher Lehrer <christopher.lehrer@students.plymouth.ac.uk>
2018-04-15 22:15:01 +0100

    added content to the 3 week wait page

--- BBAHealth/BBAHealth/Controllers/HomePageVC/ViewController.swift
+++ BBAHealth/BBAHealth/Controllers/HomePageVC/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
    
     @IBOutlet weak var collectionView: UICollectionView!
     
-    let mainMenu = ["Treatment Plan Infomation", "Side Effects Of RadioTherapy", "How RadioTherapy Works?", "Finances"]
+    let mainMenu = ["Treatment Plan Information", "Side Effects Of RadioTherapy", "How RadioTherapy Works?", "Finances"]
     
     let menuTextColourIndex = ["light","light","light","light"]
     
